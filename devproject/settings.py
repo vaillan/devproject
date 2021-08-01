5f93f08667e096e267edd0eb3dace74dcda688b3
Valentin Ortiz Santiago <red.line.v.o.s@outlook.com>
2021-08-01 15:25:10 -0500

    Correction

--- devproject/settings.py
+++ devproject/settings.py
@@ -18,15 +18,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '^05tyosw6wsfl^t#1zdlr$9c$hwne=6x=7j$uy03obq7(lst-m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
